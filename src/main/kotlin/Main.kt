b8b807bc541bf4bef8fbcac499204ea5aa81f278
Marcelo Santos <rocha.mer21@gmail.com>
2023-02-01 10:04:29 -0300

    GlobalScope

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+fun main(args: Array<String>) {
+   GlobalScope.launch {
+       delay(1000)
+       println("World")
+   }
+    println("Hello, ")
+    Thread.sleep(2000)
+}
