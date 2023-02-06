843c48b8b9e89b27c3fb4e65ad42b410ed449cdc
Marcelo Santos <rocha.mer21@gmail.com>
2023-02-05 23:39:28 -0300

    BufferedChannels

---
+++ src/main/kotlin/channels/BufferedChannels﻿.kt
@@ -0,0 +1,26 @@
+package channels
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        val channel = Channel<Int>(4) // create buffered channel
+        val sender = launch { // launch sender coroutine
+            repeat(10) {
+                println("Sending $it") // print before sending each element
+                channel.send(it) // will suspend when buffer is full
+            }
+        }
+// don't receive anything... just wait....
+        repeat(3) {
+            delay(1000)
+            println("Received ${channel.receive()}")
+            println("Received ${channel.receive()}")
+        }
+
+        sender.cancel() // cancel sender coroutine
+    }
+}
