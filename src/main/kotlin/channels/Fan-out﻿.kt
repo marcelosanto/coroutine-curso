a73ee4713d176ec99e910fa738fe1ad3e5531f69
Marcelo Santos <rocha.mer21@gmail.com>
2023-02-05 23:25:31 -0300

    Fan out

---
+++ src/main/kotlin/channels/Fan-out﻿.kt
@@ -0,0 +1,31 @@
+package channels
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        val producer = produceXNumbers()
+        repeat(5) { launchProcessor(it, producer) }
+        delay(950)
+        producer.cancel() // cancel producer coroutine and thus kill them all
+    }
+}
+
+fun CoroutineScope.produceXNumbers() = produce<Int> {
+    var x = 1 // start from 1
+    while (true) {
+        send(x++) // produce next
+        delay(100) // wait 0.1s
+    }
+}
+
+fun CoroutineScope.launchProcessor(id: Int, channel: ReceiveChannel<Int>) = launch {
+    for (msg in channel) {
+        println("Processor #$id received $msg")
+    }
+}
